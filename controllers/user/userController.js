deeeb81c55cc497337fee19eb06523d7dff6cd3a
xm343 <abhijithsivadas01@gmail.com>
2026-03-20 10:34:11 +0530

    new

--- controllers/user/userController.js
+++ controllers/user/userController.js
@@ -1,3 +1,14 @@
+
+
+
+
+function generateOtp(){
+    
+}
+
+
+
+
 const errorPage = async (req, res) => {
     try {
         res.render('page-404')
@@ -7,6 +18,15 @@ const errorPage = async (req, res) => {
     }
 }
 
+const signup = async(req,res)=>{
+    try {
+        res.render('signup')
+    } catch (error) { 
+        res.status(500).redirect('/errorPage')
+        console.log('could not render signup ',error)     
+    }
+}
+
 module.exports = {
-    errorPage
+    errorPage,signup
 }
